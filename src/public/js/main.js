85584be624060cb2f3e30f613ca8c8d891ab85ae
cesar <dev@cesar.sh>
2020-06-04 21:00:43 +0100

    use backward compatible JS syntax

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,9 +1,12 @@
-var clip = new ClipboardJS('#copy')
-var trigger = document.getElementById('copy')
-clip.on('success', function (e) {
-  trigger.className = 'fa fa-check'
-  setTimeout(function() {
-    trigger.className = 'fa fa-copy'
-  }, 500)
-  e.clearSelection()
-})
+"use strict";
+
+var target = new ClipboardJS("#copy");
+var trigger = document.getElementById("copy");
+
+target.on("success", function (e) {
+  trigger.className = "fa fa-check";
+  setTimeout(function () {
+    trigger.className = "fa fa-copy";
+  }, 500);
+  e.clearSelection();
+});
